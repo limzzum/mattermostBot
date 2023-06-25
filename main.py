21b606a58f3c45451de1a69ff956906b6f849981
limzzum <chfhddl1234@naver.com>
2023-06-25 15:37:09 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+from datetime import datetime
+
+def send_mattermost_message(webhook_url, message):
+    payload = {
+        'text': message
+    }
+    response = requests.post(webhook_url, json=payload)
+
+    if response.status_code == 200:
+        print('Message sent successfully to Mattermost!')
+    else:
+        print('Failed to send message to Mattermost.')
+
+def schedule_multiple_notifications(webhook_url, messages, times):
+    scheduler = BlockingScheduler()
+
+    for i, message in enumerate(messages):
+        time = times[i]
+        scheduler.add_job(send_mattermost_message, 'cron', day_of_week='mon-fri', hour=time.hour, minute=time.minute, args=[webhook_url, message])
+
+    scheduler.start()
+
+# Mattermost 웹훅 URL 및 전송할 메시지, 반복 시간 설정
+webhook_url = 'https://meeting.ssafy.com/hooks/m6375679i7g7xna5g4b5iryhzo'
+messages = ['입실 체크 하세요', '퇴실 체크 하세요']
+times = [datetime.strptime(time, '%H:%M') for time in ['08:50','18:00']]
+
+schedule_multiple_notifications(webhook_url, messages, times)
